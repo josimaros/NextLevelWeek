aac9b9616ed30d826f413c6a9acd27d006556c5c
Josimar Silva <josimar.os23@gmail.com>
2020-06-06 07:33:36 -0300

    page login finalizada

---
+++ web/src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (<header><h1>Ecoleta</h1></header>);
+}
+
+export default Header;
